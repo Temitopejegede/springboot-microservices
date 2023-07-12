edcbc392403d07f7e2f25ceaa00f5d1980f32823
temitopejegede <temiteejegede@gmail.com>
2023-07-12 02:06:04 -0400

    department rest controller

---
+++ department-service/src/main/java/com/temi/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,28 @@
+package com.temi.departmentservice.controller;
+
+import com.temi.departmentservice.dto.DepartmentDto;
+import com.temi.departmentservice.service.DepartmentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/departments")
+@AllArgsConstructor
+public class DepartmentController {
+
+    private DepartmentService departmentService;
+
+    //save department rest api
+
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(
+            @RequestBody DepartmentDto departmentDto){
+        DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
+        return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+    }
+}
