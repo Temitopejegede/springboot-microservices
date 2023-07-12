1689b1a19f9af8c838e1a0cbff3048d3e56a3e1e
temitopejegede <temiteejegede@gmail.com>
2023-07-12 02:05:43 -0400

    data transfer object

---
+++ department-service/src/main/java/com/temi/departmentservice/dto/DepartmentDto.java
@@ -0,0 +1,19 @@
+package com.temi.departmentservice.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor@AllArgsConstructor
+public class DepartmentDto {
+
+    private Long id;
+    private String departName;
+    private String departmentDescription;
+    private String departmentCode;
+
+}
