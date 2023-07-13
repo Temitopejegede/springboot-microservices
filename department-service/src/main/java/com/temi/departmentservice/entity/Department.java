f2e80c019c4b9ee5aa8cb0b58e698890b8b3f5dc
temitopejegede <temiteejegede@gmail.com>
2023-07-13 03:43:51 -0400

    department name and code should be unique

--- department-service/src/main/java/com/temi/departmentservice/entity/Department.java
+++ department-service/src/main/java/com/temi/departmentservice/entity/Department.java
@@ -17,10 +17,10 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String departName;
     private  String departmentDescription;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String departmentCode;
 
 
