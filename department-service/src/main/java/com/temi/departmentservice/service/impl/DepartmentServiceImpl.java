e247effd597e1d6de8016afe68e589e3b5e8f957
temitopejegede <temiteejegede@gmail.com>
2023-07-13 03:43:23 -0400

    refactored to use auto mapper

--- department-service/src/main/java/com/temi/departmentservice/service/impl/DepartmentServiceImpl.java
+++ department-service/src/main/java/com/temi/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -2,6 +2,7 @@ package com.temi.departmentservice.service.impl;
 
 import com.temi.departmentservice.dto.DepartmentDto;
 import com.temi.departmentservice.entity.Department;
+import com.temi.departmentservice.mapper.AutoDepartmentMapper;
 import com.temi.departmentservice.repository.DepartmentRepository;
 import com.temi.departmentservice.service.DepartmentService;
 import lombok.AllArgsConstructor;
@@ -18,21 +19,25 @@ public class DepartmentServiceImpl implements DepartmentService {
     public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
         //
 
-        Department department = new Department(
-                departmentDto.getId(),
-                departmentDto.getDepartName(),
-                departmentDto.getDepartmentDescription(),
-                departmentDto.getDepartmentCode()
-        );
+//        Department department = new Department(
+//                departmentDto.getId(),
+//                departmentDto.getDepartName(),
+//                departmentDto.getDepartmentDescription(),
+//                departmentDto.getDepartmentCode()
+//        );
+
+        Department department = AutoDepartmentMapper.MAPPER.mapToDepartment(departmentDto);
 
         Department savedDepartment = departmentRepository.save(department);
 
-        DepartmentDto savedDepartmentDto = new DepartmentDto(
-                department.getId(),
-                departmentDto.getDepartName(),
-                departmentDto.getDepartmentDescription(),
-                departmentDto.getDepartmentCode()
-        );
+//        DepartmentDto savedDepartmentDto = new DepartmentDto(
+//                department.getId(),
+//                departmentDto.getDepartName(),
+//                departmentDto.getDepartmentDescription(),
+//                departmentDto.getDepartmentCode()
+//        );
+
+        DepartmentDto savedDepartmentDto = AutoDepartmentMapper.MAPPER.mapToDepartmentDto(savedDepartment);
 
 
         return savedDepartmentDto;
@@ -43,12 +48,14 @@ public class DepartmentServiceImpl implements DepartmentService {
 
         Department department = departmentRepository.findByDepartmentCode(departmentCode);
 
-        DepartmentDto departmentDto = new DepartmentDto(
-                department.getId(),
-                department.getDepartName(),
-                department.getDepartmentDescription(),
-                department.getDepartmentCode()
-        );
+//        DepartmentDto departmentDto = new DepartmentDto(
+//                department.getId(),
+//                department.getDepartName(),
+//                department.getDepartmentDescription(),
+//                department.getDepartmentCode()
+//        );
+
+        DepartmentDto departmentDto = AutoDepartmentMapper.MAPPER.mapToDepartmentDto(department);
 
 
         return departmentDto;
