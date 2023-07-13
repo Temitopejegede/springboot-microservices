672060c0ab2da917790791bfb96d4447cfd61d47
temitopejegede <temiteejegede@gmail.com>
2023-07-13 03:38:08 -0400

    created auto mapper

---
+++ department-service/src/main/java/com/temi/departmentservice/mapper/AutoDepartmentMapper.java
@@ -0,0 +1,18 @@
+package com.temi.departmentservice.mapper;
+
+import com.temi.departmentservice.dto.DepartmentDto;
+import com.temi.departmentservice.entity.Department;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AutoDepartmentMapper {
+
+    AutoDepartmentMapper MAPPER = Mappers.getMapper(AutoDepartmentMapper.class);
+
+    DepartmentDto mapToDepartmentDto(Department department);
+
+    Department mapToDepartment(DepartmentDto departmentDto);
+
+
+}
