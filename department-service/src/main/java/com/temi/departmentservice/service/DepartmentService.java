433e633059e6ba2b06b8541878d157b935641003
temitopejegede <temiteejegede@gmail.com>
2023-07-14 14:03:21 -0400

    added new apis

--- department-service/src/main/java/com/temi/departmentservice/service/DepartmentService.java
+++ department-service/src/main/java/com/temi/departmentservice/service/DepartmentService.java
@@ -2,9 +2,19 @@ package com.temi.departmentservice.service;
 
 import com.temi.departmentservice.dto.DepartmentDto;
 
+import java.util.List;
+
 public interface DepartmentService {
 
     DepartmentDto saveDepartment(DepartmentDto departmentDto);
 
     DepartmentDto getDepartmentByCode(String code);
+
+    List<DepartmentDto> getAllDepartments();
+
+    void deleteDepartment(Long id);
+
+    void deleteAllDepartments();
+
+    DepartmentDto updateDepartment(DepartmentDto departmentDto);
 }
--- department-service/src/main/java/com/temi/departmentservice/service/impl/DepartmentServiceImpl.java
+++ department-service/src/main/java/com/temi/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -2,62 +2,111 @@ package com.temi.departmentservice.service.impl;
 
 import com.temi.departmentservice.dto.DepartmentDto;
 import com.temi.departmentservice.entity.Department;
+import com.temi.departmentservice.exception.DepartmentCodeAlreadyExistsException;
+import com.temi.departmentservice.exception.DepartmentNameAlreadyExistsException;
+import com.temi.departmentservice.exception.ResourceNotFoundException;
 import com.temi.departmentservice.mapper.AutoDepartmentMapper;
 import com.temi.departmentservice.repository.DepartmentRepository;
 import com.temi.departmentservice.service.DepartmentService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
-@AllArgsConstructor
+//@AllArgsConstructor
 public class DepartmentServiceImpl implements DepartmentService {
-   private DepartmentRepository departmentRepository;
 
+   private final DepartmentRepository departmentRepository;
 
+    public DepartmentServiceImpl(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
 
     @Override
     public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
-        //
+
+        Optional<Department> optionalDepartment = departmentRepository.findByDepartmentCode(departmentDto.getDepartmentCode());
+
+        if(optionalDepartment.isPresent()){
+            if(optionalDepartment.get().getDepartmentCode().equals(departmentDto.getDepartmentCode())){
+                throw  new DepartmentCodeAlreadyExistsException("Department Code Already Exists");
+            }
+            else {
+                throw new DepartmentNameAlreadyExistsException("Department Name Already Exists");
+            }
+        }
 
 //        Department department = new Department(
 //                departmentDto.getId(),
-//                departmentDto.getDepartName(),
+//                departmentDto.getDepartmentName(),
 //                departmentDto.getDepartmentDescription(),
 //                departmentDto.getDepartmentCode()
 //        );
-
         Department department = AutoDepartmentMapper.MAPPER.mapToDepartment(departmentDto);
 
         Department savedDepartment = departmentRepository.save(department);
 
 //        DepartmentDto savedDepartmentDto = new DepartmentDto(
-//                department.getId(),
-//                departmentDto.getDepartName(),
-//                departmentDto.getDepartmentDescription(),
-//                departmentDto.getDepartmentCode()
+//                savedDepartment.getId(),
+//                savedDepartment.getDepartmentName(),
+//                savedDepartment.getDepartmentDescription(),
+//                savedDepartment.getDepartmentCode()
 //        );
-
-        DepartmentDto savedDepartmentDto = AutoDepartmentMapper.MAPPER.mapToDepartmentDto(savedDepartment);
-
-
-        return savedDepartmentDto;
+    return AutoDepartmentMapper.MAPPER.mapToDepartmentDto(savedDepartment);
     }
 
     @Override
     public DepartmentDto getDepartmentByCode(String departmentCode) {
 
-        Department department = departmentRepository.findByDepartmentCode(departmentCode);
+        Department department = departmentRepository.findByDepartmentCode(departmentCode)
+                .orElseThrow(() -> new ResourceNotFoundException("department", "departmentCode", departmentCode));
 
 //        DepartmentDto departmentDto = new DepartmentDto(
 //                department.getId(),
-//                department.getDepartName(),
+//                department.getDepartmentName(),
 //                department.getDepartmentDescription(),
 //                department.getDepartmentCode()
 //        );
+        return AutoDepartmentMapper.MAPPER.mapToDepartmentDto(department);
+    }
 
-        DepartmentDto departmentDto = AutoDepartmentMapper.MAPPER.mapToDepartmentDto(department);
+    @Override
+    public List<DepartmentDto> getAllDepartments() {
+
+        List<Department> departments = departmentRepository.findAll();
+
+       return departments.stream().map((AutoDepartmentMapper.MAPPER::mapToDepartmentDto)).collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteDepartment(Long id) {
+        Department department = departmentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("department", "departmentCode", id.toString()));
+
+        departmentRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteAllDepartments() {
+        departmentRepository.deleteAll();
+    }
 
 
-        return departmentDto;
+    @Override
+    public DepartmentDto updateDepartment(DepartmentDto departmentDto) {
+        Department existingDepartment = departmentRepository.findById(departmentDto.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("department", "departmentCode", departmentDto.getDepartmentCode()));
+
+        existingDepartment.setDepartmentName(departmentDto.getDepartmentName());
+        existingDepartment.setDepartmentCode(departmentDto.getDepartmentCode());
+        existingDepartment.setDepartmentDescription(departmentDto.getDepartmentDescription());
+
+        Department updatedDepartment = departmentRepository.save(existingDepartment);
+
+        return AutoDepartmentMapper.MAPPER.mapToDepartmentDto(updatedDepartment);
     }
 }
