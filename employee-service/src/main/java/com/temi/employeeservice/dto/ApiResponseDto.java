c98e9c29c13dc0d15ca48a70c1fd3b30d2ef9747
temitopejegede <temiteejegede@gmail.com>
2023-07-15 03:58:58 -0400

    new response class -- combines employee dto and department dtp

---
+++ employee-service/src/main/java/com/temi/employeeservice/dto/ApiResponseDto.java
@@ -0,0 +1,30 @@
+package com.temi.employeeservice.dto;
+
+public class ApiResponseDto {
+    private EmployeeDto employeeDto;
+    private DepartmentDto departmentDto;
+
+    public ApiResponseDto() {
+    }
+
+    public ApiResponseDto(EmployeeDto employeeDto, DepartmentDto departmentDto) {
+        this.employeeDto = employeeDto;
+        this.departmentDto = departmentDto;
+    }
+
+    public EmployeeDto getEmployeeDto() {
+        return employeeDto;
+    }
+
+    public void setEmployeeDto(EmployeeDto employeeDto) {
+        this.employeeDto = employeeDto;
+    }
+
+    public DepartmentDto getDepartmentDto() {
+        return departmentDto;
+    }
+
+    public void setDepartmentDto(DepartmentDto departmentDto) {
+        this.departmentDto = departmentDto;
+    }
+}
