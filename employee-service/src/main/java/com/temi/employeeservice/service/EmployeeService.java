1369a702ddc477b7e8d5c06ec0bd6ba2bdbbfd1c
temitopejegede <temiteejegede@gmail.com>
2023-07-14 20:58:37 -0400

    new endpoints and implementation

--- employee-service/src/main/java/com/temi/employeeservice/service/EmployeeService.java
+++ employee-service/src/main/java/com/temi/employeeservice/service/EmployeeService.java
@@ -2,8 +2,20 @@ package com.temi.employeeservice.service;
 
 import com.temi.employeeservice.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto saveEmployee(EmployeeDto employeeDto);
 
+    List<EmployeeDto> saveEmployees(List<EmployeeDto> employeeDtos);
+
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(EmployeeDto employeeDto);
+
+    void deleteEmployee(Long id);
+
+    void deleteAllEmployees();
 }
--- employee-service/src/main/java/com/temi/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/temi/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -2,53 +2,108 @@ package com.temi.employeeservice.service.impl;
 
 import com.temi.employeeservice.dto.EmployeeDto;
 import com.temi.employeeservice.entity.Employee;
+import com.temi.employeeservice.exceptions.EmailAlreadyExistsException;
+import com.temi.employeeservice.exceptions.ResourceNotFoundException;
+import com.temi.employeeservice.mapper.AutoEmployeeMapper;
 import com.temi.employeeservice.repository.EmployeeRepository;
 import com.temi.employeeservice.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
-@AllArgsConstructor
+//@AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
 
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
+//        Employee employee = new Employee(
+//                employeeDto.getId(),
+//                employeeDto.getFirstName(),
+//                employeeDto.getLastName(),
+//                employeeDto.getEmail()
+//        );
+        Optional<Employee> optionalUser = employeeRepository.findByEmail(employeeDto.getEmail());
 
-        Employee employee = new Employee(
-                employeeDto.getId(),
-                employeeDto.getFirstName(),
-                employeeDto.getLastName(),
-                employeeDto.getEmail()
-        );
+        if(optionalUser.isPresent()){
+            throw new EmailAlreadyExistsException("Email already exists for a user");
+        }
 
+        Employee employee = AutoEmployeeMapper.MAPPER.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
+//        EmployeeDto savedEmployeeDto = new EmployeeDto(
+//                savedEmployee.getId(),
+//                savedEmployee.getFirstName(),
+//                savedEmployee.getLastName(),
+//                savedEmployee.getEmail()
+//        );
+        return AutoEmployeeMapper.MAPPER.mapToEmployeeDto(savedEmployee);
 
-        EmployeeDto savedEmployeeDto = new EmployeeDto(
-                savedEmployee.getId(),
-                savedEmployee.getFirstName(),
-                savedEmployee.getLastName(),
-                savedEmployee.getEmail()
-        );
+    }
 
-        return savedEmployeeDto;
+    @Override
+    public List<EmployeeDto> saveEmployees(List<EmployeeDto> employeeDtos) {
+        List<Employee> employees = employeeDtos.stream().map(AutoEmployeeMapper.MAPPER::mapToEmployee).toList();
 
+        List<Employee> savedEmployees = employeeRepository.saveAll(employees);
+
+        return savedEmployees.stream().map(AutoEmployeeMapper.MAPPER::mapToEmployeeDto).collect(Collectors.toList());
     }
 
     @Override
     public EmployeeDto getEmployeeById(Long employeeId) {
 
-        Employee employee = employeeRepository.findById(employeeId).get();
-
-        EmployeeDto employeeDto = new EmployeeDto(
-                employee.getId(),
-                employee.getFirstName(),
-                employee.getLastName(),
-                employee.getEmail()
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee", "id", employeeId)
         );
-
-        return employeeDto;
+//        EmployeeDto employeeDto = new EmployeeDto(
+//                employee.getId(),
+//                employee.getFirstName(),
+//                employee.getLastName(),
+//                employee.getEmail()
+//        );\
+        return AutoEmployeeMapper.MAPPER.mapToEmployeeDto(employee);
     }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+
+        return employees.stream().map(AutoEmployeeMapper.MAPPER::mapToEmployeeDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(EmployeeDto employeeDto) {
+        Employee existingEmployee = employeeRepository.findById(employeeDto.getId()).orElseThrow();
+
+        existingEmployee.setFirstName(employeeDto.getFirstName());
+        existingEmployee.setLastName(employeeDto.getLastName());
+        existingEmployee.setEmail(employeeDto.getEmail());
+
+        Employee updatedEmployee = employeeRepository.save(existingEmployee);
+
+        return AutoEmployeeMapper.MAPPER.mapToEmployeeDto(updatedEmployee);
+    }
+
+    @Override
+    public void deleteEmployee(Long id) {
+        employeeRepository.findById(id).orElseThrow();
+        employeeRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteAllEmployees() {
+        employeeRepository.deleteAll();
+    }
+
+
 }
