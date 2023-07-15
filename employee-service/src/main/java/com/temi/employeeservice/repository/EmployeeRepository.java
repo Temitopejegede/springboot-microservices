4bc08bde11962195dcf10cf9d5dc2fbfcfa9ced6
temitopejegede <temiteejegede@gmail.com>
2023-07-14 21:00:01 -0400

    added new query

--- employee-service/src/main/java/com/temi/employeeservice/repository/EmployeeRepository.java
+++ employee-service/src/main/java/com/temi/employeeservice/repository/EmployeeRepository.java
@@ -3,6 +3,10 @@ package com.temi.employeeservice.repository;
 import com.temi.employeeservice.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findByEmail(String email);
 
 }
